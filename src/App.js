d45d577fb3afebc926a731bf216966a55687b8bc
ChoiYingY <ycying1226@gmail.com>
2023-04-06 14:09:09 -0400

    set up repo

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import logo from './logo.svg';
+import './App.css';
+
+import { Canvas } from "react-three-fiber";
+import ModelViewer from "./ModelViewer";
+
+
+// import React, { Suspense } from 'react';
+// import { useGLTF } from '@react-three/drei';
+
+// import * as THREE from 'three';
+
+// const camera = new THREE.PerspectiveCamera(
+//   45,
+//   window.innerWidth / window.innerHeight,
+//   0.1,
+//   1000
+// );
+
+// function Model() { 
+//   const usingGlft = useGLTF('./wolfie.glb',true);
+//   return (
+//     <>
+//       <ambientLight intensity={1} />
+//       <spotLight intensity={0.5} angle={0.1} penumbra={1} position={[10, 15, 10]} castShadow />
+//       <pointLight position={[-10, -10, -10]} />
+//       <Suspense fallback={null}>
+//         <primitive object={usingGlft.scene} />
+//       </Suspense>
+//     </>
+//   )
+// }
+
+function App() {
+  return (
+    <ModelViewer scale="40" modelPath={"./wolfie.glb"} />
+
+    // <Canvas style={{ background: "#B6977D", height:"500px"}}>
+    // </Canvas>
+    // <div className="App">
+    //   {/* <ModelViewer scale="40" modelPath={"/rsrc/wolfie.glb"} /> */}
+    //   {/* <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header> */}
+    // </div>
+  );
+}
+
+export default App;
